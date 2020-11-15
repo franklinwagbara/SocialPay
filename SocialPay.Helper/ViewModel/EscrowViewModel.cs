1e1ee98a3fec14f005b0739fa4b3b0d81803b574
Patricksfffff <Patricksf@HQ-TECH-L202ff>
2020-11-16 00:19:28 +0100

    Modified escrow report

--- SocialPay.Helper/ViewModel/EscrowViewModel.cs
+++ SocialPay.Helper/ViewModel/EscrowViewModel.cs
@@ -25,6 +25,8 @@ namespace SocialPay.Helper.ViewModel
         public string PaymentLinkUrl { get; set; }
         public string Document { get; set; }
         public string CustomerEmail { get; set; }
+        public string Fullname { get; set; }
+        public string PhoneNumber { get; set; }
         public string Channel { get; set; }
         public string OrderStatus { get; set; }
         public string Message { get; set; }
