40a616ca48738b039dfe9ba77ba87d83d63bd8e7
Patricksfffff <Patricksf@HQ-TECH-L202ff>
2020-11-15 13:36:13 +0100

    Modified invoice table info

---
+++ SocialPay.Domain/Migrations/20201115123456_modified_invoice_info_table.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SocialPay.Domain.Migrations
+{
+    public partial class modified_invoice_info_table : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentReference",
+                table: "InvoicePaymentInfo",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PaymentReference",
+                table: "InvoicePaymentInfo");
+        }
+    }
+}
