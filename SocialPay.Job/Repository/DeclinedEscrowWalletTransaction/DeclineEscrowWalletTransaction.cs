a022ed6d03e047bdc5b4521181e20ec2b7ac58ba
Patricksfffff <Patricksf@HQ-TECH-L202ff>
2020-11-18 20:28:27 +0100

    Modified notification service

---
+++ SocialPay.Job/Repository/DeclinedEscrowWalletTransaction/DeclineEscrowWalletTransaction.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SocialPay.Domain;
+using SocialPay.Helper;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialPay.Job.Repository.DeclinedEscrowWalletTransaction
+{
+    public class DeclineEscrowWalletTransaction
+    {
+        private readonly DeclineEscrowWalletPendingTransaction _transactions;
+        public DeclineEscrowWalletTransaction(DeclineEscrowWalletPendingTransaction transactions, IServiceProvider services)
+        {
+            Services = services;
+            _transactions = transactions;
+        }
+
+        public IServiceProvider Services { get; }
+
+        public async Task<string> GetPendingTransactions()
+        {
+            try
+            {
+                //  _log4net.Info("Tasks starts to fetch awaiting transactions" + " | " + DateTime.Now);
+                using (var scope = Services.CreateScope())
+                {
+                    var context = scope.ServiceProvider.GetRequiredService<SocialPayDbContext>();
+                    DateTime nextDay = DateTime.Now.Date.AddDays(1);
+                    var pendingTransactions = await context.TransactionLog
+                        //.Where(x => x.TransactionJourney == TransactionJourneyStatusCodes.FioranoFirstFundingCompleted
+                        //|| x.TransactionJourney == TransactionJourneyStatusCodes.AwaitingCustomerFeedBack
+                        ////|| x.TransactionJourney == TransactionJourneyStatusCodes.CompletedDeliveryDayWalletFunding
+                        //&& x.TransactionStatus == OrderStatusCode.Approved
+                        .Where(x => x.TransactionStatus == OrderStatusCode.Decline
+                        ).ToListAsync();
+                    var getEscrowTransactions = pendingTransactions.Where(x => x.LinkCategory == MerchantPaymentLinkCategory.Escrow
+                    || x.LinkCategory == MerchantPaymentLinkCategory.OneOffEscrowLink).ToList();
+                    // _log4net.Info("Total number of pending transactions" + " | " + pendingTransactions.Count + " | " + DateTime.Now);
+                    if (getEscrowTransactions.Count == 0)
+                        return "No record";
+                    await _transactions.ProcessTransactions(pendingTransactions);
+                    //return "No record";
+                }
+
+                Console.WriteLine("GenerateDailyReport : " + DateTime.Now.ToString());
+
+                return "GenerateDailyReport";
+            }
+            catch (Exception ex)
+            {
+                //  _log4net.Error("An error occured while fetching awaiting transactions" + " | " + ex.Message.ToString() + " | " + DateTime.Now);
+                return "Error";
+            }
+
+        }
+    }
+}
