f77f5873e37c91bc0d17589acaac793f9b9e8bb0
Patricksfffff <Patricksf@HQ-TECH-L202ff>
2020-11-20 09:06:46 +0100

    Activated escrow declined service

---
+++ SocialPay.Job/TaskSchedules/DeclinedEscrowWalletTask.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SocialPay.Job.Repository.AcceptedEscrowOrdersWalletTransaction;
+using SocialPay.Job.Repository.DeclinedEscrowWalletTransaction;
+using SocialPay.Job.Repository.PayWithCard;
+using SocialPay.Job.Services;
+using System;
+using System.Threading.Tasks;
+
+namespace SocialPay.Job.TaskSchedules
+{
+    public class DeclinedEscrowWalletTask : ScheduledProcessor
+    {
+        public DeclinedEscrowWalletTask(IServiceScopeFactory serviceScopeFactory) : base(serviceScopeFactory)
+        {
+        }
+
+        protected override string Schedule => "*/" + 5 + " * * * *"; // every 4 min 
+
+        public override Task ProcessInScope(IServiceProvider scopeServiceProvider)
+        {
+            IDeclineEscrowWalletTransaction reportGenerator = scopeServiceProvider.GetRequiredService<IDeclineEscrowWalletTransaction>();
+            reportGenerator.GetPendingTransactions();
+            return Task.CompletedTask;
+        }
+    }
+}
