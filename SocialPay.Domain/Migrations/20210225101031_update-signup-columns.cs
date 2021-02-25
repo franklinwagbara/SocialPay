faf2f876362b6d0aa968ffa5f6ddf137bfbaca33
Patricksfffff <Patricksf@HQ-TECH-L202ff>
2021-02-25 11:47:12 +0100

    Modified table columns

---
+++ SocialPay.Domain/Migrations/20210225101031_update-signup-columns.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SocialPay.Domain.Migrations
+{
+    public partial class updatesignupcolumns : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Tin",
+                table: "MerchantBusinessInfo",
+                type: "NVARCHAR(40)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR(20)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "ClientAuthentication",
+                type: "NVARCHAR(20)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR(15)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FullName",
+                table: "ClientAuthentication",
+                type: "NVARCHAR(55)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR(45)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Tin",
+                table: "MerchantBusinessInfo",
+                type: "NVARCHAR(20)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR(40)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "ClientAuthentication",
+                type: "NVARCHAR(15)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR(20)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FullName",
+                table: "ClientAuthentication",
+                type: "NVARCHAR(45)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR(55)",
+                oldNullable: true);
+        }
+    }
+}
