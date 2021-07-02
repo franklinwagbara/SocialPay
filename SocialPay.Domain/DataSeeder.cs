36dddd34156080a899a390652686dbe4f73046ff
festypat <festypat9@gmail.com>
2021-07-02 20:21:16 +0100

    Enabled data seed

--- SocialPay.Domain/DataSeeder.cs
+++ SocialPay.Domain/DataSeeder.cs
@@ -9,17 +9,31 @@ namespace SocialPay.Domain
         public static void Seed(SocialPayDbContext _context)
         {
             //if (!_context.ClientAuthentication.Any(x => x.Email == "festus.patrick@sterling.ng"))
-            //  {
-            //          var createuser = new ClientAuthentication[]
-            //          {
+            //{
+            //    var createuser = new ClientAuthentication[]
+            //    {
             //           new ClientAuthentication { FullName = "Festus Patrick", Email= "festus.patrick@sterling.ng",
             //          IsDeleted = false, PhoneNumber = "08030523654", LastDateModified = DateTime.Now, UserName = "patricksf",
             //          RoleName ="Super Administrator", StatusCode = "00"
             //          },
-            //          };
-            //          _context.ClientAuthentication.AddRange(createuser);
-            //          _context.SaveChanges();
-            //  }
+            //    };
+            //    _context.ClientAuthentication.AddRange(createuser);
+            //    _context.SaveChanges();
+            //}
+
+
+            if (!_context.ClientAuthentication.Any(x => x.Email == "abayomi.sawyerr@sterling.ng"))
+            {
+                var createuser = new ClientAuthentication[]
+                {
+                       new ClientAuthentication { FullName = "abayomi sawyerr", Email= "abayomi.sawyerr@sterling.ng",
+                      IsDeleted = false, PhoneNumber = "08078272823", LastDateModified = DateTime.Now, UserName = "sawyerra",
+                      RoleName ="Super Administrator", StatusCode = "00"
+                      },
+                };
+                _context.ClientAuthentication.AddRange(createuser);
+                _context.SaveChanges();
+            }
 
             if (!_context.ClientAuthentication.Any(x => x.Email == "Pelumi.Majolagbe@sterling.ng"))
             {
@@ -27,7 +41,7 @@ namespace SocialPay.Domain
                 {
                          new ClientAuthentication { FullName = "Pelumi Majolagbe", Email= "Pelumi.Majolagbe@sterling.ng",
                         IsDeleted = false, PhoneNumber = "08045646334", LastDateModified = DateTime.Now, UserName = "majolagbeip",
-                        RoleName ="Super Administrator", StatusCode = "07"
+                        RoleName ="Super Administrator", StatusCode = "00"
                         },
                 };
                 _context.ClientAuthentication.AddRange(createuser);
