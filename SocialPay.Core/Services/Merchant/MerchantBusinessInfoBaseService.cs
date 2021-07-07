836b84b5eef363347a33940ce01fa073e3ae63d9
Patricksfffff <Patricksf@HQ-TECH-L202ff>
2021-07-07 19:39:37 +0100

    Modified business info

--- SocialPay.Core/Services/Merchant/MerchantBusinessInfoBaseService.cs
+++ SocialPay.Core/Services/Merchant/MerchantBusinessInfoBaseService.cs
@@ -1,4 +1,6 @@
-﻿using SocialPay.ApplicationCore.Interfaces.Service;
+﻿using Microsoft.Extensions.Options;
+using SocialPay.ApplicationCore.Interfaces.Service;
+using SocialPay.Core.Configurations;
 using SocialPay.Helper;
 using SocialPay.Helper.Dto.Response;
 using System;
@@ -9,18 +11,23 @@ namespace SocialPay.Core.Services.Merchant
     public class MerchantBusinessInfoBaseService
     {
         private readonly IMerchantBusinessInfoService _merchantBusinessInfoService;
+        private readonly AppSettings _appSettings;
 
-        public MerchantBusinessInfoBaseService(IMerchantBusinessInfoService merchantBusinessInfoService)
+        public MerchantBusinessInfoBaseService(IMerchantBusinessInfoService merchantBusinessInfoService,
+            IOptions<AppSettings> appSettings)
         {
+            _appSettings = appSettings.Value;
             _merchantBusinessInfoService = merchantBusinessInfoService ?? throw new ArgumentNullException(nameof(merchantBusinessInfoService));
         }
 
         public async Task<WebApiResponse> GetMerchantBusinessInfoAsync(long clientId)
         {
-           // clientId = 90;
+            //clientId = 90;
             try
             {
                 var request = await _merchantBusinessInfoService.GetMerchantBusinessInfo(clientId);
+
+                request.Logo = request.Logo == string.Empty ? string.Empty : _appSettings.BaseApiUrl + request.FileLocation + "/" + request.Logo;
 
                 return new WebApiResponse { ResponseCode = AppResponseCodes.Success, Data = request };
             }
