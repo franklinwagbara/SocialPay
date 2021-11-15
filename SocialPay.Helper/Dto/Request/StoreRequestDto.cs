b3856b12659fde542191543321960fe77f52d7ba
Patricksf <Patricksf@HQ-TECH-L202>
2021-11-15 18:05:17 +0100

    Modified store service

--- SocialPay.Helper/Dto/Request/StoreRequestDto.cs
+++ SocialPay.Helper/Dto/Request/StoreRequestDto.cs
@@ -7,9 +7,12 @@ namespace SocialPay.Helper.Dto.Request
     public class StoreRequestDto
     {
         [Required(ErrorMessage = "Please enter store.")]
+        [StringLength(30, ErrorMessage = "Cannot exceed 30 characters. ")]
         public string StoreName { get; set; }
         [Required(ErrorMessage = "Please enter link.")]
+        [StringLength(30, ErrorMessage = "Cannot exceed 30 characters. ")]
         public string StoreLink { get; set; }
+        [StringLength(150, ErrorMessage = "Cannot exceed 30 characters. ")]
         public string Description { get; set; }
         [Required(ErrorMessage = "Please select image.")]
         [DataType(DataType.Upload)]
