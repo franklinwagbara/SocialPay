07d4e95c18482ee9e0072474ae7878c04b7abcc7
Patricksf <Patricksf@HQ-TECH-L202>
2021-12-06 13:44:59 +0100

    Pr to master

---
+++ SocialPay.Core/Services/SpectaOnboardingService/Services/SpectaSendEmailVerificationCode.cs
@@ -0,0 +1,89 @@
+﻿using AutoMapper;
+using SocialPay.Domain;
+using SocialPay.Helper;
+using SocialPay.Helper.Dto.Response;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialPay.Core.Services.SpectaOnboardingService.Services
+{
+    //public class SpectaSendEmailVerificationCode : ISpectaSendEmailVerificationCode
+    //{
+
+    //    private readonly SocialPayDbContext _context;
+    //    private readonly ISpectaOnBoarding _spectaOnboardingService;
+    //    private readonly IMapper _mapper;
+    //    static readonly log4net.ILog _log4net = log4net.LogManager.GetLogger(typeof(PayWithSpectaService));
+
+    //    public SpectaSendEmailVerificationCode(SocialPayDbContext context, ISpectaOnBoarding spectaOnboardingService, IMapper mapper)
+    //    {
+    //        _context = context;
+    //        _mapper = mapper;
+    //        _spectaOnboardingService = spectaOnboardingService;
+    //    }
+
+    //    public async Task<WebApiResponse> SendEmailVerificationCode(SendEmailVerificationCodeRequestDto model)
+    //    {
+    //        try
+    //        {
+    //            using (var transaction = await _context.Database.BeginTransactionAsync())
+    //            {
+    //                try
+    //                {
+    //                    var checkregistered = await _context.RegisterCustomerRequest.SingleOrDefaultAsync(x => x.emailAddress == model.email);
+
+    //                    if (checkregistered.RegistrationStatus != SpectaProcessCodes.RegisterCustomer)
+    //                        return new WebApiResponse { ResponseCode = checkregistered.RegistrationStatus, Message = "Processing stage is not Send Email Verification Code" };
+    //                    var requestmodel = _mapper.Map<SendEmailVerificationCodeRequest>(model);
+    //                    await _context.SendEmailVerificationCodeRequest.AddAsync(requestmodel);
+    //                    var request = await _spectaOnboardingService.SendEmailVerificationCode(model);
+    //                    if (request.ResponseCode != AppResponseCodes.Success)
+    //                        return request;
+    //                    var response = (SpectaResponseWithBoolResultMessage.SpectaResponseDto)request.Data;
+    //                    var sendemailverificationcoderesponse = new SendEmailVerificationCodeResponse();
+    //                    sendemailverificationcoderesponse.result = Convert.ToBoolean(response?.result);
+    //                    sendemailverificationcoderesponse.success = Convert.ToBoolean(response?.success);
+    //                    sendemailverificationcoderesponse.unAuthorizedRequest = response.unAuthorizedRequest;
+    //                    sendemailverificationcoderesponse.__abp = Convert.ToBoolean(response?.__abp);
+
+    //                    if (response.error != null)
+    //                    {
+    //                        sendemailverificationcoderesponse.code = response.error.code;
+    //                        sendemailverificationcoderesponse.details = response.error.details;
+    //                        sendemailverificationcoderesponse.message = response.error.message;
+    //                        sendemailverificationcoderesponse.validationErrors = response.error.validationErrors;
+    //                    }
+    //                    await _context.SendEmailVerificationCodeResponse.AddAsync(sendemailverificationcoderesponse);
+    //                    if (checkregistered != null)
+    //                    {
+    //                        checkregistered.RegistrationStatus = SpectaProcessCodes.SendEmailVerificationCode;
+    //                    }
+    //                    await _context.SaveChangesAsync();
+    //                    if (request.ResponseCode != AppResponseCodes.Success)
+    //                        return new WebApiResponse { ResponseCode = AppResponseCodes.Failed, Message = "Request failed", Data = request.Data };
+    //                    await transaction.CommitAsync();
+    //                    return new WebApiResponse { ResponseCode = SpectaProcessCodes.SendEmailVerificationCode, Message = "Success", Data = request.Data };
+    //                }
+    //                catch (Exception ex)
+    //                {
+    //                    await transaction.RollbackAsync();
+    //                    _log4net.Error("Error occured" + " | " + "SendEmailVerificationCode" + " | " + ex.Message.ToString() + " | " + DateTime.Now);
+    //                    return new WebApiResponse { ResponseCode = AppResponseCodes.Failed, Message = "Request failed " + ex.Message };
+
+    //                }
+    //            }
+    //        }
+    //        catch (Exception ex)
+    //        {
+
+    //            _log4net.Error("Error occured" + " | " + "SendEmailVerificationCode" + " | " + ex.Message.ToString() + " | " + DateTime.Now);
+    //            return new WebApiResponse { ResponseCode = AppResponseCodes.Failed, Message = "Request failed " + ex.Message };
+
+    //        }
+    //    }
+
+    //}
+
+}
