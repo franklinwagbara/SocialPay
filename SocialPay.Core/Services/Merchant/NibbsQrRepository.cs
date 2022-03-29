e4fe639bc17d9b4192e9ad4e26dad3fdb7d08752
Ferdinand Nnona <Ferdinand.Nnona@sterling.ng>
2022-03-29 15:10:35 +0100

    o

--- SocialPay.Core/Services/Merchant/NibbsQrRepository.cs
+++ SocialPay.Core/Services/Merchant/NibbsQrRepository.cs
@@ -225,16 +225,16 @@ namespace SocialPay.Core.Services.Merchant
 
                 var getMerchantBankInfo = await _context.MerchantBankInfo.SingleOrDefaultAsync(x => x.ClientAuthenticationId == clientId);
 
-                //model.accountName = getMerchantBankInfo.AccountName;
-                //model.accountNumber = getMerchantBankInfo.Nuban;
-                //model.bankNo = getMerchantBankInfo.BankCode;
-                //model.mchNo = nibbsMerchantInfo.MerchantQRCodeOnboardingResponse.Select(x=>x.MchNo).FirstOrDefault();
+                model.accountName = getMerchantBankInfo.AccountName;
+                model.accountNumber = getMerchantBankInfo.Nuban;
+                model.bankNo = getMerchantBankInfo.BankCode;
+                model.mchNo = nibbsMerchantInfo.MerchantQRCodeOnboardingResponse.Select(x => x.MchNo).FirstOrDefault();
 
 
-                model.accountName = "OGUNLANA TUNJI";
-                model.accountNumber = "0122047425";
-                model.bankNo = "999058";
-                model.mchNo = "M0000000105";
+                ////model.accountName = "OGUNLANA TUNJI";
+                ////model.accountNumber = "0122047425";
+                ////model.bankNo = "999058";
+                ////model.mchNo = "M0000000105";
 
                 using (var transaction = await _context.Database.BeginTransactionAsync())
                 {
